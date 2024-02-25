c05bc69d710ec32cf8a2ec9b883adfbd7a1d9020
Viranchee Lotia <viranchee@outlook.com>
2024-02-24 21:18:05 -0500

    --allow-empty-message

--- modernCpp.cpp
+++ modernCpp.cpp
@@ -10,26 +10,10 @@ Something new: spaceship operator <=>
 #include <iostream>
 #include <stdio.h>
 
-// #include "llvm
-void ch1();
+#include "Chapters/common.h"
 
 int main(int argc, char **argv) {
   ch1();
+  ch2();
   return 0;
-}
-
-void ch1() {
-  int *val = nullptr;
-  auto size = sizeof(int);
-  val = (int *)malloc(size);
-  *val = 500;
-  *val = true ? 500 : (int)NULL;
-  std::cout << "Value: " << val << "\tDeref " << &val << "\tRef " << *val
-            << std::endl;
-  // Make pointer to a pointer
-  int **ptr = &val;
-  std::cout << "Pointer: " << ptr << "\tDeref " << *ptr << "\tRef " << **ptr
-            << std::endl;
-  // Free the memory
-  free(val);
 }
