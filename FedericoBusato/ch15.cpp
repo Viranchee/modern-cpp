905706321db34df28b94ff19c0d35437f5a3268b
Viranchee Lotia <viranchee@outlook.com>
2024-03-05 15:05:42 -0500

    Ch15 (#14)

--- FedericoBusato/ch15.cpp
+++ FedericoBusato/ch15.cpp
@@ -1,1 +1,30 @@
-void ch15() {}
+#include <iostream>
+
+using namespace std;
+
+namespace {
+void cmake() {
+  //
+}
+
+void doxygen() {
+  /// Comments are Doxygen compatible
+  // Prefer clang-doc
+}
+void stats() {
+  //
+}
+void otherTools() {
+  // Code search: ripgrep / Hypergrep : 100x faster than grep
+  // FIRACODE: Font with ligatures
+}
+
+} // namespace
+
+void ch15() {
+  cout << "ch15" << endl;
+  cmake();
+  doxygen();
+  stats();
+  otherTools();
+}
