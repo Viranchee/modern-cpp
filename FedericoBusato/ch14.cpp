f4fdb0f32113f18e871d555bf2b0e6a165527a71
Viranchee Lotia <viranchee@outlook.com>
2024-03-05 08:30:33 -0500

    ch14

--- FedericoBusato/ch14.cpp
+++ FedericoBusato/ch14.cpp
@@ -1,1 +1,53 @@
-void ch14() {}
+#include <iostream>
+
+using namespace std;
+namespace {
+void debugging() {
+  //
+}
+void assertions() {
+  //
+}
+void execDebugging() {
+  //
+}
+void memoryDebugging() {
+  //
+}
+void hardeningTechniques() {
+  //
+}
+void sanitizers() {
+  //
+}
+void debuggingSummary() {
+  //
+}
+void compilerWarnings() {
+  //
+}
+void staticAnalysis() {
+  //
+}
+void codeTesting() {
+  //
+}
+void codeQuality() {
+  //
+}
+} // namespace
+
+void ch14() {
+  cout << "ch14" << endl;
+  debugging();
+  assertions();
+  execDebugging();
+  memoryDebugging();
+  hardeningTechniques();
+  sanitizers();
+  debuggingSummary();
+  compilerWarnings();
+  staticAnalysis();
+  codeTesting();
+  codeQuality();
+}
